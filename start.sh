81c1ad6cb40d4dc283c4236d2719dfe681a50f70
patri8625 <patrisevero@gmail.com>
2025-08-14 20:04:38 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "=== Test: alleen Caddy starten ==="
-caddy version
+echo "=== Start Tailscale ==="
+tailscaled &
+
+sleep 5
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME}
+
+echo "=== Genereer Caddyfile ==="
+cat <<EOF > /etc/caddy/Caddyfile
+:8080 {
+  reverse_proxy ${TAILSCALE_HA_SCHEME}://${TAILSCALE_HA_HOST}:${TAILSCALE_HA_PORT}
+}
+EOF
+
+echo "=== Start Caddy ==="
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
